9a51993ea74f5a4f8f98fecba51a60ecf8f10a75
Omkar <omkarak45@gmail.com>
2021-11-02 00:11:04 +0530

    add:env and all

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -76,7 +76,10 @@ export const createApolloClient = ({
 	})
 
 	const uploadLink = new createUploadLink({
-		uri: ssrMode ? 'http://localhost:5000/graphql' : API_URL,
+		uri: 'https://social-media-backend-production.up.railway.app/graphql',
+		// process.env.NODE_ENV === 'development'
+		// 	? 'http://localhost:5000/graphql'
+		// 	: API_URL,
 		headers: headers,
 		credentials: 'include',
 	})
