1da906a517e2df96de98fed8cb50f1fcaf619271
Omkar <omkarak45@gmail.com>
2021-11-08 11:40:23 +0530

    add: umami analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,12 @@ export default class MyDocument extends NextDocument {
 				<Head>
 					{/* Splitbee Analytics */}
 					<script async src="https://cdn.splitbee.io/sb.js"></script>
+					<script
+						async
+						defer
+						data-website-id="e992ccdb-5de4-4063-a5ff-f74e5bd829b2"
+						src="https://umami-production-01a2.up.railway.app/umami.js"
+					></script>
 				</Head>
 				<body>
 					<Main />
